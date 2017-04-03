5d0fd658602fade0dce6c32d598497cc37791350
Maggie Harrington <maggie.harrington@gmail.com>
2017-04-03 16:16:59 -0700

    create mock-products.ts, import Product model and move array of Products here from MarketplaceComponent

---
+++ src/app/mock-products.ts
@@ -0,0 +1,10 @@
+import { Product } from './product.model';
+
+export const PRODUCTS: Product[] = [
+  new Product("Product1", "Manufacturer1", "Description for Product 1, this is a really great item.", 1),
+  new Product("Product2", "Manufacturer2", "Description for Product 2, this is a really great item.", 2),
+  new Product("Product3", "Manufacturer3", "Description for Product 3, this is a really great item.", 3),
+  new Product("Product4", "Manufacturer4", "Description for Product 4, this is a really great item.", 4),
+  new Product("Product5", "Manufacturer5", "Description for Product 5, this is a really great item.", 5),
+  new Product("Product6", "Manufacturer6", "Description for Product 6, this is a really great item.", 6)
+];
